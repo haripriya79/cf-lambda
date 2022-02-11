d33b232dec885fb0ac3242f61f37e622a6a66471
Haripriya Gangavaram <gangavaramharipriya@gmail.com>
2022-02-11 16:54:39 +0530

    lambda with cf

---
+++ dynamodb/index.js
@@ -0,0 +1,74 @@
+'use strict';
+
+const AWS = require("aws-sdk");
+AWS.config.update({
+    region: "us-east-1",
+
+});
+
+exports.handler = async(event, context) => {
+    const dynamo = new AWS.DynamoDB.DocumentClient();
+    let body;
+    let statusCode = 200;
+    const headers = {
+        "Content-Type": "application/json"
+    };
+
+    try {
+        switch (event.routeKey) {
+            case "DELETE /testDynamodb/students/{id}":
+                await dynamo
+                    .delete({
+                        TableName: "student",
+                        Key: {
+                            id: event.pathParameters.id
+                        }
+                    })
+                    .promise();
+                body = `Deleted item ${event.pathParameters.id}`;
+                break;
+            case "GET /getstudents/{id}":
+                body = await dynamo
+                    .get({
+                        TableName: "student",
+                        Key: {
+                            id: event.pathParameters.id
+                        }
+                    })
+                    .promise();
+                break;
+            case "GET /getstudents":
+                body = await dynamo.scan({ TableName: "student" }).promise();
+                break;
+            case "PUT /testDynamodb/students":
+                let requestJSON = JSON.parse(event.body);
+                await dynamo
+                    .put({
+                        TableName: "student",
+                        Item: {
+                            id: requestJSON.id,
+                            firstname: requestJSON.firstname,
+                            lastname: requestJSON.lastname,
+                            age: requestJSON.age,
+                            branch: requestJSON.branch,
+                        }
+                    })
+                    .promise();
+                body = `Put item ${requestJSON.id}`;
+                break;
+            default:
+                throw new Error(`Unsupported route: "${event.routeKey}"`);
+        }
+    } catch (err) {
+        statusCode = 400;
+        body = err.message;
+    } finally {
+        body = JSON.stringify(body);
+    }
+
+    return {
+        statusCode,
+        body,
+        headers
+    };
+};
